5bf84190a01f862f2cae5d59dab46a0bf497efd9
Jorge H. F. Pereira <jorge.h.f.pereira@gmail.com>
2013-04-21 17:59:41 +0200

    Initial project commit

---
+++ Phase2_ColourConnection/doc_log/doc.md
@@ -0,0 +1,5 @@
+Fachhochschule Köln - Campus Gummersbach
+Web-basierte Anwendung 2: Verteilte Systeme  
+
+#Dokumentation
+
---
+++ Phase2_ColourConnection/src/de/fhkoeln/gm/wba2/phase2/ColourConnectionMain.java
@@ -0,0 +1,13 @@
+package de.fhkoeln.gm.wba2.phase2;
+
+public class ColourConnectionMain {
+
+	/**
+	 * @author Jorge H. F. Pereira
+	 */
+	public static void main(String[] args) {
+		
+
+	}
+
+}
